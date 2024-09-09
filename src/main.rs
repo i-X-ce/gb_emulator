7bc67dbd4217afbc1043cbf8f25970a02643503d
i-X-ce <iceci2804@gmail.com>
2024-09-10 03:05:22 +0900

    チュートリアル00

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("y is {}", y);
+    }
+    println!("y is {}", y);
+
+    let mut some_strings = "aaa";
+    println!("strings is {}", some_strings);
+
+    let some_strings = some_strings.len();
+    println!("string len is {}", some_strings);
 }
