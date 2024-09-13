e0e36610833647f2050fcae49dad095557fc31bf
i-X-ce <iceci2804@gmail.com>
2024-09-14 02:02:44 +0900

    -#032

--- src/main.rs
+++ src/main.rs
@@ -126,6 +126,8 @@ impl CPU {
         match instruction {
             Instruction::NOP => self.pc,
             Instruction::LD(load_type) => self.ld(load_type),
+            Instruction::INC(target) => self.inc(target),
+            Instruction::DEC(target) => self.dec(target),
             Instruction::ADD(target) => self.add(target),
             Instruction::JP(test) => self.jump(test),
             Instruction::JPHL => self.jumphl(),
@@ -141,18 +143,9 @@ impl CPU {
     fn ld(&mut self, load_type: LoadType) -> u16{
         match load_type {
             LoadType::Byte(target, source) => {
-                let source_value = self.read_registers_byte(source);
-                match target {
-                    ArithmeticTarget::A => self.registers.a = source_value,
-                    ArithmeticTarget::B => self.registers.b = source_value,
-                    ArithmeticTarget::C => self.registers.c = source_value,
-                    ArithmeticTarget::D => self.registers.d = source_value,
-                    ArithmeticTarget::E => self.registers.e = source_value,
-                    ArithmeticTarget::H => self.registers.h = source_value,
-                    ArithmeticTarget::L => self.registers.l = source_value,
-                    ArithmeticTarget::HL_ => self.bus.write_byte(self.registers.get_hl(), source_value),
-                    _ => { panic!("TODO: inplement other targets")}
-                };
+                let source_value = self.read_registers_arithmeticTarget(source);
+                self.change_registers_arithmeticTarget(target, source_value);
+                
                 match source {
                     ArithmeticTarget::D8 => self.pc.wrapping_add(2),
                     _ => self.pc.wrapping_add(1),
@@ -165,8 +158,8 @@ impl CPU {
                 };
                 match target {
                     ArithmeticTarget::BC => self.registers.set_bc(source_value),
-                    ArithmeticTarget::DE => self.registers.set_bc(source_value),
-                    ArithmeticTarget::HL_ => self.registers.set_bc(source_value),
+                    ArithmeticTarget::DE => self.registers.set_de(source_value),
+                    ArithmeticTarget::HL => self.registers.set_hl(source_value),
                     _ => { panic!("TODO: inplement other targets")}
                 };
                 self.pc.wrapping_add(3)
@@ -175,6 +168,50 @@ impl CPU {
         } 
     }
     
+    fn inc(&mut self, target: ArithmeticTarget) -> u16 {
+        let is8bit = self.is_8bit(target);
+        let value = self.read_registers_arithmeticTarget(target);
+        match is8bit {
+            true => {
+                let (new_value, did_overflow) = (value as u8).overflowing_add(1);
+                self.change_flag(
+                    new_value == 0, 
+                    false, 
+                    value & 0x0F == 0x0F, 
+                    self.registers.f.carry);
+                    self.change_registers_arithmeticTarget(target, new_value as u16);
+                },
+
+            false => {
+                let (new_value, did_overflow) = value.overflowing_add(1);
+                self.change_registers_arithmeticTarget(target, new_value);
+            }
+        };
+        self.pc.wrapping_add(1)
+    }
+
+    fn dec(&mut self, target: ArithmeticTarget) -> u16 {
+        let is8bit = self.is_8bit(target);
+        let value = self.read_registers_arithmeticTarget(target);
+        match is8bit {
+            true => {
+                let (new_value, did_overflow) = (value as u8).overflowing_sub(1);
+                self.change_flag(
+                    new_value == 0, 
+                    true, 
+                    value & 0x0F == 0x00, 
+                    self.registers.f.carry);
+                    self.change_registers_arithmeticTarget(target, new_value as u16);
+                },
+
+            false => {
+                let (new_value, did_overflow) = value.overflowing_sub(1);
+                self.change_registers_arithmeticTarget(target, new_value);
+            }
+        };
+        self.pc.wrapping_add(1)
+    }
+
     fn add (&mut self, target: ArithmeticTarget) -> u16 {
         match target {
             ArithmeticTarget::SP => {
@@ -189,7 +226,7 @@ impl CPU {
                 self.pc.wrapping_add(2)
             },
             _ => {
-                let value = self.read_registers_byte(target);
+                let value = self.read_registers_arithmeticTarget(target) as u8;
                 let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
                 self.registers.f.zero = new_value == 0;
                 self.registers.f.subtract = false;
@@ -207,7 +244,7 @@ impl CPU {
     }
 
     fn addhl(&mut self, target: ArithmeticTarget) -> u16 {
-        let value = self.read_registers_word(target);
+        let value = self.read_registers_arithmeticTarget(target);
         let (new_value, did_overflow) = self.registers.get_hl().overflowing_add(value);
         self.registers.f.subtract = false;
         self.registers.f.carry = did_overflow;
@@ -236,6 +273,7 @@ impl CPU {
             StackTarget::BC => self.registers.get_bc(),
             StackTarget::DE => self.registers.get_de(),
             StackTarget::HL => self.registers.get_hl(),
+            StackTarget::D16(address) => address,
             _ => { panic!("TODO: support more targets") }
         };
         self.sp = self.sp.wrapping_sub(1);
@@ -272,6 +310,7 @@ impl CPU {
 
         if self.should_jump(test) {
             self.push(StackTarget::D16((next_pc)));
+            self.pc.wrapping_sub(1);
             self.read_next_word()
         } else {
             next_pc
@@ -313,34 +352,103 @@ impl CPU {
         (u << 8) | l
     }
 
-    fn read_registers_byte(&self, target: ArithmeticTarget) -> u8{
+    //レジスタやメモリの値を持ってくる
+    fn read_registers_arithmeticTarget(&mut self, target: ArithmeticTarget) -> u16{
         match target {
-            ArithmeticTarget::A => self.registers.a,
-            ArithmeticTarget::B => self.registers.b,
-            ArithmeticTarget::C => self.registers.c,
-            ArithmeticTarget::D => self.registers.d,
-            ArithmeticTarget::E => self.registers.e,
-            ArithmeticTarget::H => self.registers.h,
-            ArithmeticTarget::L => self.registers.l,
-            ArithmeticTarget::HL_ => self.bus.read_byte(self.registers.get_hl()),
-            ArithmeticTarget::HLi_ => self.bus.read_byte(self.registers.get_hl()),
-            ArithmeticTarget::HLd_ => self.bus.read_byte(self.registers.get_hl()),
-            ArithmeticTarget::BC_ => self.bus.read_byte(self.registers.get_bc()),
-            ArithmeticTarget::DE_ => self.bus.read_byte(self.registers.get_de()),
-            ArithmeticTarget::D8 => self.read_next_byte(),
-            ArithmeticTarget::D16_ => self.bus.read_byte(self.read_next_word()),
-            _ => panic!("TODO: support more targets")
-        }
-    }
-
-    fn read_registers_word(&self, target: ArithmeticTarget) -> u16{
-        match target {
+            ArithmeticTarget::A => self.registers.a as u16,
+            ArithmeticTarget::B => self.registers.b as u16,
+            ArithmeticTarget::C => self.registers.c as u16,
+            ArithmeticTarget::D => self.registers.d as u16,
+            ArithmeticTarget::E => self.registers.e as u16,
+            ArithmeticTarget::H => self.registers.h as u16,
+            ArithmeticTarget::L => self.registers.l as u16,
+            ArithmeticTarget::HL_ => self.bus.read_byte(self.registers.get_hl()) as u16,
+            ArithmeticTarget::HLi_ => {
+                let value = self.bus.read_byte(self.registers.get_hl()) as u16;
+                self.registers.set_hl((self.registers.get_hl()).wrapping_add(1));
+                value
+            },
+            ArithmeticTarget::HLd_ => {
+                let value = self.bus.read_byte(self.registers.get_hl()) as u16;
+                self.registers.set_hl((self.registers.get_hl()).wrapping_sub(1));
+                value
+            },
+            ArithmeticTarget::BC_ => self.bus.read_byte(self.registers.get_bc()) as u16,
+            ArithmeticTarget::DE_ => self.bus.read_byte(self.registers.get_de()) as u16,
+            ArithmeticTarget::D8 => self.read_next_byte() as u16,
+            ArithmeticTarget::D16_ => self.bus.read_byte(self.read_next_word()) as u16,
+            //16bit
             ArithmeticTarget::BC => self.registers.get_bc(),
             ArithmeticTarget::DE => self.registers.get_de(),
             ArithmeticTarget::HL => self.registers.get_hl(),
             ArithmeticTarget::SP => self.sp,
             _ => panic!("TODO: support more targets")
         }
+    }
+
+    // レジスタやメモリへの代入を代行する
+    fn change_registers_arithmeticTarget(&mut self, target: ArithmeticTarget, value: u16){
+        match target {
+            ArithmeticTarget::A => self.registers.a = value as u8,
+            ArithmeticTarget::B => self.registers.b = value as u8,
+            ArithmeticTarget::C => self.registers.c = value as u8,
+            ArithmeticTarget::D => self.registers.d = value as u8,
+            ArithmeticTarget::E => self.registers.e = value as u8,
+            ArithmeticTarget::H => self.registers.h = value as u8,
+            ArithmeticTarget::L => self.registers.l = value as u8,
+            ArithmeticTarget::BC_ => self.bus.write_byte(self.registers.get_bc(), value as u8),
+            ArithmeticTarget::DE_ => self.bus.write_byte(self.registers.get_de(), value as u8),
+            ArithmeticTarget::HL_ => self.bus.write_byte(self.registers.get_hl(), value as u8),
+            ArithmeticTarget::HLi_ => { 
+                self.bus.write_byte(self.registers.get_hl(), value as u8); 
+                self.registers.set_hl((self.registers.get_hl()).wrapping_add(1));
+            },
+            ArithmeticTarget::HLd_ => { 
+                self.bus.write_byte(self.registers.get_hl(), value as u8); 
+                self.registers.set_hl((self.registers.get_hl()).wrapping_sub(1));
+            },
+            // 16bit
+            ArithmeticTarget::BC => self.registers.set_bc(value),
+            ArithmeticTarget::DE => self.registers.set_de(value),
+            ArithmeticTarget::HL => self.registers.set_hl(value),
+            _ => panic!("TODO: support more targets")
+        };
+    }
+
+    fn is_8bit(&mut self, target: ArithmeticTarget) -> bool {
+        match target {
+            ArithmeticTarget::A => true,
+            ArithmeticTarget::B => true,
+            ArithmeticTarget::C => true,
+            ArithmeticTarget::D => true,
+            ArithmeticTarget::E => true,
+            ArithmeticTarget::H => true,
+            ArithmeticTarget::L => true,
+            ArithmeticTarget::D8 => true,
+            ArithmeticTarget::BC_ => true,
+            ArithmeticTarget::DE_ => true,
+            ArithmeticTarget::HL_ => true,
+            ArithmeticTarget::HLi_ => true,
+            ArithmeticTarget::HLd_ => true,
+            ArithmeticTarget::D16_ => true,
+            ArithmeticTarget::FD8_ => true,
+            ArithmeticTarget::FDC_ => true,
+
+            ArithmeticTarget::BC => false,
+            ArithmeticTarget::DE => false,
+            ArithmeticTarget::HL => false,
+            ArithmeticTarget::SP => false,
+            ArithmeticTarget::D16 => false,
+            ArithmeticTarget::SPA => false,
+            _ => panic!("TODO: support more targets")
+        }
+    }
+
+    fn change_flag(&mut self, zero: bool, subtract: bool, half_carry: bool, carry: bool){
+        self.registers.f.zero = zero;
+        self.registers.f.subtract = subtract;
+        self.registers.f.half_carry = half_carry;
+        self.registers.f.carry = carry;
     }
 
     fn should_jump(&self, test: JumpTest) -> bool {
@@ -385,6 +493,158 @@ mod test {
             half_carry,
             carry
         }
+    }
+
+    #[test]
+    fn test_inc(){
+        // B
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x04);
+        cpu.registers.b = 0x00;
+        cpu.step();
+        assert_eq!(cpu.registers.b, 0x01);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
+
+        // B zero
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x04);
+        cpu.registers.b = 0xFF;
+        cpu.step();
+        assert_eq!(cpu.registers.b, 0x00);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(true, false, true, false)
+        );
+
+        // (HL)
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x34);
+        cpu.bus.write_byte(0x1000, 0x00);
+        cpu.registers.set_hl(0x1000);
+        cpu.step();
+        assert_eq!(cpu.bus.read_byte(0x1000), 0x01);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
+
+        // BC
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x03);
+        cpu.registers.set_bc(0x1000);
+        cpu.step();
+        assert_eq!(cpu.registers.get_bc(), 0x1001);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
+
+        // BC 8
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x03);
+        cpu.registers.set_bc(0x00FF);
+        cpu.step();
+        assert_eq!(cpu.registers.get_bc(), 0x0100);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
+
+        // BC over
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x03);
+        cpu.registers.set_bc(0xFFFF);
+        cpu.step();
+        assert_eq!(cpu.registers.get_bc(), 0x0000);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
+    }
+
+    #[test]
+    fn test_dec(){
+        // B
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x05);
+        cpu.registers.b = 0x02;
+        cpu.step();
+        assert_eq!(cpu.registers.b, 0x01);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, true, false, false)
+        );
+
+        // B zero
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x05);
+        cpu.registers.b = 0x01;
+        cpu.step();
+        assert_eq!(cpu.registers.b, 0x00);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(true, true, false, false)
+        );
+
+        // (HL)
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x35);
+        cpu.bus.write_byte(0x1000, 0x02);
+        cpu.registers.set_hl(0x1000);
+        cpu.step();
+        assert_eq!(cpu.bus.read_byte(0x1000), 0x01);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, true, false, false)
+        );
+
+        // BC
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x0B);
+        cpu.registers.set_bc(0x1002);
+        cpu.step();
+        assert_eq!(cpu.registers.get_bc(), 0x1001);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
+
+        // BC 8
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x0B);
+        cpu.registers.set_bc(0x0100);
+        cpu.step();
+        assert_eq!(cpu.registers.get_bc(), 0x00FF);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
+
+        // BC over
+        let mut cpu = CPU::new();
+        cpu.bus.write_byte(0x0000, 0x0B);
+        cpu.registers.set_bc(0x0000);
+        cpu.step();
+        assert_eq!(cpu.registers.get_bc(), 0xFFFF);
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(
+            cpu.registers.f,
+            F(false, false, false, false)
+        );
     }
 
     #[test]
